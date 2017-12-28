6e704b0ece926bc382904830f51724f38bed2e7b
kaliumxyz <martijn@becker.wtf>
2017-12-28 16:53:49 +0100

    fix(example) add logging to choice

--- examples/choice.js
+++ examples/choice.js
@@ -1,13 +1,14 @@
 #!/bin/env node
 "use strict";
 const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
 
 const Connection = require('../');
 
 /* logging */
 const logStream = fs.createWriteStream(path.join(__dirname, `choose.log`), { flags: 'a' });
 function log(...text) {
-		console.log(text)
 		text.forEach(text => {
 			logStream.write(`${Date.now()} - ${JSON.stringify(text)}\n`)
 		})
@@ -19,15 +20,19 @@ const room = (process.argv.join().match(/-r,(\w+)/) || [,'test'])[1]
 
 const connection = new Connection(room);
 
-// on a broadcast, reply to the frigging post with one random choice
-connection.on('broadcast', ev => {
-	log(ev)
-	if(ev.data.type === 'post' && ev.data.text)
-		if(ev.data.text.startsWith('.choose')){
-			const options = ev.data.text
-				.substring(7)
-				.split(',');
-			log(options);
-			connection.post(options[Math.floor(Math.random() * options.length)], ev.id);
-		}
+connection.once('ready', ev => {
+	connection.nick('choice');
+
+	// on a broadcast, reply to the frigging post with one random choice
+	connection.on('broadcast', ev => {
+		log(ev)
+		if(ev.data.type === 'post' && ev.data.text)
+			if(ev.data.text.startsWith('.choose')){
+				const options = ev.data.text
+					.substring(7)
+					.split(',');
+				log(options);
+				connection.post(options[Math.floor(Math.random() * options.length)], ev.id);
+			}
+	});
 });
