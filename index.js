d72a15a824c55eacd3d4881026b51e75a4cadf1a
kaliumxyz <martijn@becker.wtf>
2018-03-17 23:55:16 +0100

    fix() who reply must be unicasted to a valid ID

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-'use strict'
-const ws = require('ws')
-/* TODO:
- * - uuid.
- */
+'use strict';
+const ws = require('ws');
+
 class connection extends ws {
 	constructor(room = 'welcome', uri = 'https://instant.leet.nu', options = { origin: 'instant.leet.nu' }, ...callback) {
-		super(uri + '/room/' + room + '/ws', options)
-		this.room = room
-		this.seq = 0
-		this.id = ''
-		this.uuid = ''
-		this.response = []
+		super(uri + '/room/' + room + '/ws', options);
+		this.room = room;
+		this.seq = 0;
+		this.id = '';
+		this.uuid = '';
+		this._response = [];
 		// Setting the basics for the connection.
 		this.once('open', data => {
-			callback.forEach(f => f(data))
-			this.emit('ready')
-			this.on('message', this._handleMsg)
-			this.on('response', this._handleResponse)
-		})
+			callback.forEach(f => f(data));
+			this.emit('ready');
+			this.on('message', this._handleMsg);
+			this.on('response', this._handleResponse);
+		});
 		this.once('identity', data => {
-			this.id = data.id
-			this.uuid = data.uuid
-		})
+			data = data.data;
+			this.id = data.id;
+			this.uuid = data.uuid;
+		});
 		this.on('who', data => {
 			this.send(JSON.stringify({
 				type: 'unicast',
+				to: data.from,
 				seq: this.seq++,
 				data: {
 					type: 'nick',
-					nick: this.nick
+					nick: this._nick,
+					uuid: this.uuid
 				}
-			}))
-		})
+			}));
+		});
 	}
 
 	_handleMsg(data, flags) {
-		const dt = JSON.parse(data)
-		this.emit(dt.type, dt)
-		if(dt.type === 'broadcast')
-			this.emit(dt.data.type, dt.data)
+		try {
+			const dt = JSON.parse(data);
+			this.emit(dt.type, dt);
+			if(dt.type === 'broadcast')
+				this.emit(dt.data.type, dt);
+		} catch (e) {
+			console.log({error: e, data: data});
+		}
 	}
-
+	
 	post(msg, parent = null, ...callback) {
 
-		this._queueResponse(callback)
+		this._queueResponse(callback);
 
 		this.send(JSON.stringify({
 			type: 'broadcast',
@@ -54,13 +59,13 @@ class connection extends ws {
 				text: msg,
 				parent: parent
 			}
-		}))
+		}));
 
 	}
 
 	pm(msg, recipient, ...callback) {
 
-		this._queueResponse(callback)
+		this._queueResponse(callback);
 
 		this.send(JSON.stringify({
 			type: 'unicast',
@@ -72,7 +77,7 @@ class connection extends ws {
 				text: msg,
 				type: 'privmsg',
 			}
-		}))
+		}));
 
 	}
 
@@ -80,24 +85,24 @@ class connection extends ws {
 		this.send(JSON.stringify({
 			seq: this.seq++,
 			type: 'ping'
-		}))
+		}));
 
 		this.once('pong', data => {
-			callback.forEach(f => f())
-		})
+			callback.forEach(f => f());
+		});
 	}
 	who(...callback) {
 		this.send(JSON.stringify({
 			type: 'broadcast',
 			seq: this.seq++,
 			data: {type: 'who'}
-		}))
-		callback.forEach(f => f())
+		}));
+		callback.forEach(f => f());
 	}
 
 	nick(nick, ...callback) {
 
-		this._queueResponse(callback)
+		this._queueResponse(callback);
 
 		this.send(JSON.stringify({
 			type: 'broadcast',
@@ -107,20 +112,20 @@ class connection extends ws {
 				uuid: this.uuid,
 				nick: nick
 			}
-		}))
+		}));
 
-		this.nick = nick
+		this._nick = nick;
 
 	}
 
 	_queueResponse(callback) {
-		this.response[this.seq] = callback
+		this._response[this.seq] = callback;
 	}
 
 	_handleResponse(data) {
-		this.response[data.seq].forEach(f => f(data))
+		this._response[data.seq].forEach(f => f(data));
 	}
 
 }
 
-module.exports = connection
+module.exports = connection;
